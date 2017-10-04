93cf004ad7ff4c85e19ce58f23f47a6245c55cef
petar-staynov <petar.staynov@gmail.com>
2017-10-05 01:37:13 +0300

    Lab - MathPotatoe

--- StacksAndQueues/HotPotato/Program.cs
+++ StacksAndQueues/HotPotato/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HotPotato
 {
@@ -7,7 +8,33 @@ namespace HotPotato
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            string input = Console.ReadLine();
+            var queue = new Queue<string>(input.Split(' '));
+            int n = int.Parse(Console.ReadLine());
+
+
+            while (queue.Count > 1)
+            {
+                for (int i = 0; i < n-1; i++)
+                {
+                    string reminder = queue.Dequeue();
+                    queue.Enqueue(reminder);
+                }
+                Console.WriteLine("Removed " + queue.Dequeue());
+            }
+            Console.WriteLine("Last is " + queue.Dequeue());
         }
     }
 }
+
+/*
+Shuffles until n-th element is first and removes him
+
+test inputs:
+Mimi Pepi Toshko
+2
+
+Gosho Pesho Misho Stefan Krasi
+10
+
+*/
---
+++ StacksAndQueues/MathPotato/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HotPotato
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var queue = new Queue<string>(input.Split(' '));
+            int n = int.Parse(Console.ReadLine());
+
+
+            int cycle = 1;
+            while (queue.Count > 1)
+            {
+                for (int i = 0; i < n-1; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+                if (IsPrime(cycle))
+                {
+                    Console.WriteLine("Prime " + queue.Peek());
+                }
+                else
+                {
+                    Console.WriteLine("Removed " + queue.Dequeue());
+                }
+                cycle++;
+
+            }
+            Console.WriteLine("Last is " + queue.Dequeue());
+        }
+        
+        public static bool IsPrime(int number)
+        {
+            if (number == 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0)  return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i+=2)
+            {
+                if (number % i == 0)  return false;
+            }
+
+            return true;        
+        }
+    }
+    
+}
+
+/*
+Shuffles until n-th element is first and removes him
+
+test inputs:
+Mimi Pepi Toshko
+2
+
+Gosho Pesho Misho Stefan Krasi
+10
+
+*/
