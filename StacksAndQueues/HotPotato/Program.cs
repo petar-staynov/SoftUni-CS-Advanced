2f3e5c52a0d5193220082e79c36592729e96af0f
petar-staynov <petar.staynov@gmail.com>
2017-10-04 17:45:37 +0300

    Initial commit

---
+++ StacksAndQueues/HotPotato/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotPotato
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StacksAndQueues
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            return;
+        }
+    }
+}
