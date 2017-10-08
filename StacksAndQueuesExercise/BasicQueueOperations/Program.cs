523cb770f054eb90ecbb5d8a69ccc301ce2488c7
petar-staynov <petar.staynov@gmail.com>
2017-10-08 18:41:22 +0300

    stacks and queues - 4,5,6

--- StacksAndQueuesExercise/BasicQueueOperations/Program.cs
+++ StacksAndQueuesExercise/BasicQueueOperations/Program.cs
@@ -7,7 +7,56 @@ namespace BasicQueueOperations
     {
         public static void Main(string[] args)
         {
+            var input = Console.ReadLine().Split(' ');
+            var elements = Console.ReadLine().Split(' ');
             
+            var amount = Convert.ToInt32(input[0]);
+            var remove = Convert.ToInt32(input[1]);
+            var exists = Convert.ToInt32(input[2]);
+
+            if (amount <= remove)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+            
+            var queue = new Queue<int>();
+            for (int i = 0; i < amount; i++)
+            {
+                queue.Enqueue(Convert.ToInt32(elements[i]));
+                
+            }
+
+            for (int i = 0; i < remove; i++)
+            {
+                queue.Dequeue();
+            }
+            if (queue.Contains(exists))
+            {
+                Console.WriteLine("true");
+                return;
+            }
+            var smallest = int.MaxValue;
+            foreach (var element in queue)
+            {
+                //var elementValue = queue.Dequeue();
+                if (element < smallest)
+                {
+                    smallest = element;
+                }
+            }
+            Console.WriteLine(smallest);
         }
     }
 }
+
+/*
+5 2 32
+1 13 45 32 4
+
+4 1 666
+666 69 13 420
+
+3 3 90
+90 90 90
+*/
---
+++ StacksAndQueuesExercise/SequenceWithQueue/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SequenceWithQueue
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var s1 = long.Parse(Console.ReadLine());
+
+            var queue = new Queue<long>();
+            var result = new List<long>();
+
+            queue.Enqueue(s1);
+            result.Add(s1);
+            //Console.Write(s1 + " ");
+            for (int i = 0; i < 20; i++)
+            {
+                var queueHead = queue.Dequeue();
+
+                var ele2 = queueHead + 1;
+                queue.Enqueue(ele2);
+                result.Add(ele2);
+                //Console.Write(ele2 + " ");
+
+                var ele3 = 2 * queueHead + 1;
+                queue.Enqueue(ele3);
+                result.Add(ele3);
+                //Console.Write(ele3 + " ");
+
+                var ele4 = queueHead + 2;
+                queue.Enqueue(ele4);
+                result.Add(ele4);
+                //Console.Write(ele4 + " ");
+            }
+
+            for (int i = 0; i < 50; i++)
+            {
+                Console.Write(result[i] + " ");
+            }
+        }
+    }
+}
+/*
+s1 = 2 s1
+s2 = 3 s1 +1
+s3 = 5 2s1+1
+s4 = 4 s1+2
+s5 = 4 s2+1
+s6 = 7 2s2+1
+s7 = 5 s2 + 1 
+s8 = 6 s3 + 1
+11 
+7 
+5 
+9 
+6
+
+*/
